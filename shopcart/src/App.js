4d3588e837d9401fdbd0d705db5a8d0944fa0893
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-10-30 11:43:02 -0700

    copy everything from Codesandbox

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemList: [
+        {
+          id: 1,
+          image: "./products/cologne.jpg",
+          desc: "Unisex Cologne",
+          value: 1
+        },
+        {
+          id: 2,
+          image: "./products/iwatch.jpg",
+          desc: "Apple iWatch",
+          value: 1
+        },
+        {
+          id: 3,
+          image: "./products/mug.jpg",
+          desc: "Unique Mug",
+          value: 8
+        },
+        {
+          id: 4,
+          image: "./products/wallet.jpg",
+          desc: "Mens Wallet",
+          value: 5
+        }
+      ]
+    };
+  }
+
+  render() {
+    const tValue = this.state.itemList.map((Value) => {
+      return Value.value;
+    });
+    let totalValue = 0;
+    for (let i = 0; i < tValue.length; i++) {
+      totalValue += tValue[i];
+    }
+    tValue.reduce((totalValue, tValue) => totalValue + tValue, 0);
+    console.log(totalValue);
+
+    let shoppingItem = <Shopping shoppinglists={this.state.itemList} />;
+
+    return (
+      <div className="App">
+        <div id="header">
+          <h1>Shop to React</h1>
+          <div id="total-items">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            <div id="total-value"> {totalValue} </div>
+            items
+          </div>
+        </div>
+        {shoppingItem}
+      </div>
+    );
+  }
+}
+
+function Shopping(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ListGroup>
+      {props.shoppinglists.map((shoppinglist) => (
+        <ListGroupItem key={shoppinglist.id}>
+          <h5>{shoppinglist.desc}</h5>
+          <img src={shoppinglist.image} alt="product img"></img>
+          <span>{shoppinglist.value}</span>
+          quantity
+        </ListGroupItem>
+      ))}
+    </ListGroup>
   );
 }
 
